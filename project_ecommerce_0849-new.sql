cef9810033acc87b14d5ad63ac2c748a1a29d48a
marufaziz <azizmuzani@gmail.com>
2020-04-11 02:19:41 +0700

    new db

---
+++ project_ecommerce_0849-new.sql
@@ -0,0 +1,370 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2020 at 09:16 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project_ecommerce_0849`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(2) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `username`, `password`, `nama`, `email`) VALUES
+(1, 'admin', 'admin', 'Marcheilla', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_biaya_kirim`
+--
+
+CREATE TABLE `tbl_biaya_kirim` (
+  `id_biaya_kirim` int(11) NOT NULL,
+  `biaya` int(50) NOT NULL,
+  `id_kota` int(11) NOT NULL,
+  `id_jasa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_biaya_kirim`
+--
+
+INSERT INTO `tbl_biaya_kirim` (`id_biaya_kirim`, `biaya`, `id_kota`, `id_jasa`) VALUES
+(3, 15000, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_detail_order`
+--
+
+CREATE TABLE `tbl_detail_order` (
+  `id_detail_order` int(11) NOT NULL,
+  `id_order` int(5) NOT NULL,
+  `id_produk` int(5) NOT NULL,
+  `jumlah` int(5) NOT NULL,
+  `harga` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(3) NOT NULL,
+  `nama_kategori` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'pakaian pria '),
+(3, 'Pakaian wanita'),
+(4, 'Aksesoris'),
+(5, 'Kebutuhan Sekolah'),
+(6, 'Elektronik'),
+(7, 'Buku');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kota`
+--
+
+CREATE TABLE `tbl_kota` (
+  `id_kota` int(11) NOT NULL,
+  `nama_kota` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kota`
+--
+
+INSERT INTO `tbl_kota` (`id_kota`, `nama_kota`) VALUES
+(1, 'madiun'),
+(2, 'klaten');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kurir`
+--
+
+CREATE TABLE `tbl_kurir` (
+  `id_kurir` int(11) NOT NULL,
+  `nama_kurir` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kurir`
+--
+
+INSERT INTO `tbl_kurir` (`id_kurir`, `nama_kurir`) VALUES
+(1, 'JNE'),
+(2, 'WAHANA'),
+(3, 'TIKI');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_member`
+--
+
+CREATE TABLE `tbl_member` (
+  `id_member` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_hp` varchar(18) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_merek`
+--
+
+CREATE TABLE `tbl_merek` (
+  `id_merek` int(3) NOT NULL,
+  `nama_merek` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_merek`
+--
+
+INSERT INTO `tbl_merek` (`id_merek`, `nama_merek`) VALUES
+(1, '3Seconds'),
+(2, 'nevada');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id_order` int(5) NOT NULL,
+  `status_order` char(1) NOT NULL,
+  `tgl_order` date NOT NULL,
+  `jam_order` time NOT NULL,
+  `id_member` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(10) NOT NULL,
+  `id_kategori_produk` int(3) NOT NULL,
+  `id_merek` int(3) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `deskripsi` text,
+  `harga` int(20) NOT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  `slide` char(1) DEFAULT 'N',
+  `rekomendasi` char(1) DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `id_kategori_produk`, `id_merek`, `nama_produk`, `deskripsi`, `harga`, `gambar`, `slide`, `rekomendasi`) VALUES
+(1, 5, 2, 'kemeja keren', 'kemeja cowo masa kinni', 150000, '114835logo_pb.png', 'N', 'Y'),
+(2, 3, 1, 'kemeja', 'sss', 150000, '103116logo_amikom_satu_warna.png', 'N', 'Y');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tbl_biaya_kirim`
+--
+ALTER TABLE `tbl_biaya_kirim`
+  ADD PRIMARY KEY (`id_biaya_kirim`),
+  ADD KEY `id_jasa` (`id_jasa`),
+  ADD KEY `id_kota` (`id_kota`);
+
+--
+-- Indexes for table `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  ADD PRIMARY KEY (`id_detail_order`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_kota`
+--
+ALTER TABLE `tbl_kota`
+  ADD PRIMARY KEY (`id_kota`);
+
+--
+-- Indexes for table `tbl_kurir`
+--
+ALTER TABLE `tbl_kurir`
+  ADD PRIMARY KEY (`id_kurir`);
+
+--
+-- Indexes for table `tbl_member`
+--
+ALTER TABLE `tbl_member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `tbl_merek`
+--
+ALTER TABLE `tbl_merek`
+  ADD PRIMARY KEY (`id_merek`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id_order`);
+
+--
+-- Indexes for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `id_kategori_produk` (`id_kategori_produk`),
+  ADD KEY `id_merek` (`id_merek`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_biaya_kirim`
+--
+ALTER TABLE `tbl_biaya_kirim`
+  MODIFY `id_biaya_kirim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  MODIFY `id_detail_order` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_kota`
+--
+ALTER TABLE `tbl_kota`
+  MODIFY `id_kota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_kurir`
+--
+ALTER TABLE `tbl_kurir`
+  MODIFY `id_kurir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_member`
+--
+ALTER TABLE `tbl_member`
+  MODIFY `id_member` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_merek`
+--
+ALTER TABLE `tbl_merek`
+  MODIFY `id_merek` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id_order` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_biaya_kirim`
+--
+ALTER TABLE `tbl_biaya_kirim`
+  ADD CONSTRAINT `tbl_biaya_kirim_ibfk_1` FOREIGN KEY (`id_kota`) REFERENCES `tbl_kota` (`id_kota`),
+  ADD CONSTRAINT `tbl_biaya_kirim_ibfk_2` FOREIGN KEY (`id_jasa`) REFERENCES `tbl_kurir` (`id_kurir`);
+
+--
+-- Constraints for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD CONSTRAINT `tbl_produk_ibfk_1` FOREIGN KEY (`id_kategori_produk`) REFERENCES `tbl_kategori` (`id_kategori`),
+  ADD CONSTRAINT `tbl_produk_ibfk_2` FOREIGN KEY (`id_merek`) REFERENCES `tbl_merek` (`id_merek`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
